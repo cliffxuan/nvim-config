73d49d672f16b0e19bdc4e336427f0ae10deb0e1
Cliff Xuan <cliff.xuan@gmail.com>
2026-08-22 23:47:23 +0100

    fix: pin nvim-treesitter to master branch

--- lua/lazyvim/plugins/nvim-treesitter.lua
+++ lua/lazyvim/plugins/nvim-treesitter.lua
@@ -1,6 +1,7 @@
 return {
   {
     'nvim-treesitter/nvim-treesitter',
+    branch = 'master',
     build = ':TSUpdate',
     config = function()
       -- Compat shim for Neovim 0.12 + the frozen nvim-treesitter `master`.
